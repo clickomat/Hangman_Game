1d828da999e9a81c504beb722d94b98344fe4170
Shawn Connell <shawnconnell@204-99-162-3.uguest.utah.edu>
2018-02-03 10:14:45 -0700

    added html css and some JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+// Define Variables need for game
+var name = .prompt(Whats your Name?)
+var word = Array["range", "rustler", "doggies", "cattle", "roundup", "lasso", "chaps","colt","bandana"];
+var guessCorrect = ("yup...");
+var guessWrong= ("nope...");
+var win =0;
+var loss = 0;
+
+//set up Game works
+
+&
+
+
+// some earlier code to reference
+
+// $(document).ready(function () {
+
+//     // CREATE THE MISSING CODE HERE. Your code should add content to the random-number div.
+//     // ...
+
+//     $("#random-button").on("click", function() {
+//       var num = Math.floor(Math.random() * 100 + 1);
+//       $("#random-number").html(num);
+
+
+//       // ...
+
+//     });
+//   });
